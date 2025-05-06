00fad3c9d2c7630d77a10614ceefa0d00a65a762
Jeremy Meng <jeremy.ymeng@gmail.com>
2025-05-05 18:47:49 -0700

    [engsys] add a tool to report js repo health status
    following what python team has at https://github.com/Azure/azure-sdk-for-python/tree/main/scripts/repo_health_status_report

---
+++ eng/tools/repo-health-status-report/src/urlHelpers.js
@@ -0,0 +1,41 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+// @ts-check
+
+/**
+ * @param {string} pipelineId
+ * @return {string} - the url to get the latest build for the pipeline
+ */
+export function buildUrl(pipelineId) {
+  return `https://dev.azure.com/azure-sdk/internal/_apis/build/builds?definitions=${pipelineId}&$top=1&queryOrder=finishTimeDescending&reasonFilter=schedule&api-version=7.0`;
+}
+
+/**
+ * @param {string} buildId
+ * @return {string} - the timeline url for the build
+ */
+export function buildTimelineUrl(buildId) {
+  return `https://dev.azure.com/azure-sdk/internal/_apis/build/builds/${buildId}/Timeline?api-version=7.0`;
+}
+
+/**
+ *
+ * @param {string} label - space separated github labels
+ * @param {"bug" | "question"} kind - issue kind
+ * @param {string} created - issue created date in ISO format
+ * @returns {string} - the github issue link
+ */
+export function githubIssueLinkUrl(label, kind, created) {
+  const minus = kind === "question" ? "bug" : "question";
+  return `https://github.com/Azure/azure-sdk-for-js/issues?q=is%3Aopen+is%3Aissue+label%3Acustomer-reported+label%3AClient+-label%3Aissue-addressed+-label%3A${minus}+-label%3Aneeds-author-feedback+-label%3Afeature-request+label%3A%22${label.replace(" ", "+")}%22+created%3A%22${created}%22`;
+}
+
+/**
+ *
+ * @param {string} label - space separated github labels
+ * @returns {string} - the github issue link
+ */
+export function githubTotalIssueLink(label) {
+  return `https://github.com/Azure/azure-sdk-for-js/issues?q=is%3Aopen+is%3Aissue+label%3Acustomer-reported+label%3AClient+label%3A%22${label.replace(" ", "+")}%22`;
+}
