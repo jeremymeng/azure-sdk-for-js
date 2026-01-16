92d7b94dddf963e5af3fea7dcbb8d951e8858360
Jeremy Meng <jeremy.ymeng@gmail.com>
2026-01-16 21:59:33 +0000

    disable linting for the new generated code

--- sdk/storage/storage-blob/eslint.config.mjs
+++ sdk/storage/storage-blob/eslint.config.mjs
@@ -2,6 +2,9 @@ import azsdkEslint from "@azure/eslint-plugin-azure-sdk";
 
 export default azsdkEslint.config([
   {
+    ignores: ["./src/storage-blob-rest/**"],
+  },
+  {
     rules: {
       "@azure/azure-sdk/ts-naming-options": "warn",
       "@typescript-eslint/no-redeclare": "warn",
